bcfdbf4d8e07d2866bb70219cdbd0a20b3bdfbb7
aLeC199 <aadeyeyedavid@gmail.com>
2025-07-13 03:32:05 +0100

    Create action.js

---
+++ script/action.js
@@ -0,0 +1,14 @@
+let menu=document.getElementById("overLay"); 
+let menuFilter=document.getElementById("filter");
+
+
+function showMenu() {
+ menu.style.visibility='visible'; 
+  menuFilter.style.visibility='visible';
+}
+
+function removeMenu() {
+    menu.style.visibility='hidden';
+
+    menuFilter.style.visibility='hidden';
+}
